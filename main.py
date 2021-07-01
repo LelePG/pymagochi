81118321c452298549dba449b91027324664ce9b
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-06-30 23:59:11 -0300

    implementacao da thread de banheiro

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from manipulador_interface import altera_valores as av
 from manipulador_interface import cria_threads as ct
 from manipulador_interface import cria_elementos as ce
 from tkinter import * # importa a biblioteca gráfica
-from random import randint
 from tkinter import messagebox
+from random import randint
+import time
  
 # constantes
 TEMPO_ATUALIZAR_INTERFACE = 0.25
 TEMPO_DECREMENTAR_STATUS = 0.5
+TEMPO_VISUALIZA_CAIXA = 5
 
 ## Variáveis do programa
 pymagochi = Pymagochi("teste.png") # inicia o objeto tamagochi
@@ -39,22 +41,22 @@ label_felicidade_status = ce.criar_label(frame_principal,status[2][3],1,2)
 label_energia_status = ce.criar_label(frame_principal,status[3][3],1,3)
 label_banheiro_status = ce.criar_label(frame_principal,status[4][3],1,4)
 
+
 def monitorar_interface():
     if not pymagochi.esta_vivo():
-        print("a")
-        #messagebox.showwarning("RIP", "Você deixou o Pymagochi morrer")# mostra mensagem de erro
-        #janela.destroy()
+        messagebox.showwarning("RIP", "Você deixou o Pymagochi morrer")# mostra mensagem de erro
+        janela.destroy()
     elif pymagochi.banheiro_zerado():
         messagebox.showwarning("Aconteceu um acidente", "Limpe a caixa de areia do pymagochi")
         pymagochi.banheiro = 10
+        time.sleep(TEMPO_VISUALIZA_CAIXA)
 
 def atualizar_interface():
     label_comida_status['text'] = pymagochi.comida
     label_bebida_status['text'] = pymagochi.bebida
     label_felicidade_status['text'] = pymagochi.felicidade
     label_energia_status['text'] = pymagochi.energia
-    label_banheiro_status['text'] = pymagochi.banheiro
-    monitorar_interface()
+    label_banheiro_status['text'] = pymagochi.banheiro   
 
 def decrementar_status(): # eu queria utilizar um switch, mas não tem em python
     status_dec = randint(0,4)
@@ -71,16 +73,21 @@ def decrementar_status(): # eu queria utilizar um switch, mas não tem em python
     else:
         print("Problema encontrado")
 
+    monitorar_interface()
+
+#Threads
+thread_atualiza_interface = ct.minhaThread(TEMPO_ATUALIZAR_INTERFACE, atualizar_interface)
+thread_decrementa_status = ct.minhaThread(TEMPO_DECREMENTAR_STATUS, decrementar_status)
 
 for i in range (len(status)): # cria a interface
     label_item = ce.criar_label(frame_principal,status[i][0],0,i)
     btn_item = ce.criar_btn(frame_principal,pymagochi,status[i][1], status[i][2], 2,i)
 
-#Threads
-ct.minhaThread(TEMPO_ATUALIZAR_INTERFACE, atualizar_interface).roda_funcao()
-ct.minhaThread(TEMPO_DECREMENTAR_STATUS, decrementar_status).roda_funcao()
 
-
-##Falta: uma thread pra cada botão, verificar se o tamagochi morreu.
+thread_atualiza_interface.roda_funcao()
+thread_decrementa_status.roda_funcao()
 
 janela.mainloop()#inicia a janela
+
+
+    
--- manipulador_interface/cria_threads.py
+++ manipulador_interface/cria_threads.py
@@ -1,7 +1,7 @@
 import threading
 from threading import Timer
 
-class minhaThread (threading.Thread):
+class minhaThread(threading.Thread):
     def __init__(self, tempo_atualizacao, funcao_callback):
         threading.Thread.__init__(self)
         self.tempo_atualizacao = tempo_atualizacao
@@ -11,30 +11,3 @@ class minhaThread (threading.Thread):
         self.funcao_callback() #declara a função
         self.thread = Timer(self.tempo_atualizacao, self.roda_funcao) #cria a thread que vai rodar a função
         self.thread.start() #inicia a execução
-
-    # def run(self):
-    #     print "Iniciando thread %s com %d processos" % (self.name,self.contador)
-    #     processo(self.nome, self.contador)
-    #     print "Finalizando " + self.nome
-
-# def processo(nome, contador):
-#     while contador:
-#         print "Thread %s fazendo o processo %d" % (nome, contador)
-#         contador -= 1
-
-# # Criando as threads
-# thread1 = minhaThread(1, "Alice", 8)
-# thread2 = minhaThread(2, "Bob", 8)
-
-# # Comecando novas Threads
-# thread1.start()
-# thread2.start()
-
-# threads = []
-# threads.append(thread1)
-# threads.append(thread2)
-
-# for t in threads:
-#     t.join()
-
-# print "Saindo da main"
