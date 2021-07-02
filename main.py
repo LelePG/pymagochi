254d3c990df0e5d37e2d233c3e43c20d23519b39
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-01 23:02:12 -0300

    sons adicionados no arquivo principal

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from Pymagochi import Pymagochi
 from manipulador_interface import altera_valores as av
 from manipulador_interface import cria_threads as ct
 from manipulador_interface import cria_elementos as ce
+from manipulador_interface import toca_musica as tm
 from tkinter import * # importa a biblioteca gráfica
 from tkinter import messagebox
 from random import randint
+import time
+
 
 # constantes
 TEMPO_ATUALIZAR_INTERFACE = 0.25
@@ -42,12 +45,13 @@ label_felicidade_status = ce.criar_label(frame_principal,status[2][3],1,2)
 label_energia_status = ce.criar_label(frame_principal,status[3][3],1,3)
 label_banheiro_status = ce.criar_label(frame_principal,status[4][3],1,4)
 
-
 def monitorar_interface():
     if not pymagochi.esta_vivo():
+        tm.finalizar()
         messagebox.showwarning("RIP", "Você deixou o Pymagochi morrer")# mostra mensagem de erro
         janela.destroy()
     elif pymagochi.banheiro_zerado():
+        tm.notificar()
         messagebox.showwarning("Aconteceu um acidente", "Limpe a caixa de areia do pymagochi")
         pymagochi.banheiro = 10
         time.sleep(TEMPO_VISUALIZA_CAIXA)
@@ -75,11 +79,11 @@ def decrementar_status(): # eu queria utilizar um switch, mas não tem em python
         print("Problema encontrado")
     monitorar_interface()
 
-
 for i in range (len(status)): # cria a interface
     label_item = ce.criar_label(frame_principal,status[i][0],0,i)
     btn_item = ce.criar_btn(frame_principal,pymagochi,status[i][1], status[i][2], 2,i)
 
+janela.protocol("WM_DELETE_WINDOW", janela.destroy)#acaba o programa quando fecho a janela
 
 ct.minhaThread(TEMPO_ATUALIZAR_INTERFACE, atualizar_interface).roda_funcao()
 ct.minhaThread(TEMPO_DECREMENTAR_STATUS, decrementar_status).roda_funcao()
--- manipulador_interface/toca_musica.py
+++ manipulador_interface/toca_musica.py
@@ -4,11 +4,11 @@ import time
 def notificar():
     player = AudioPlayer("audio/mixkit-retro-game-notification-212.wav")
     player.play()
-    time.sleep(1.3)
+    time.sleep(1)
     player.stop()
 
 def finalizar():
     player = AudioPlayer("audio/mixkit-arcade-retro-game-over-213.wav")
     player.play()
-    time.sleep(1.3)
+    time.sleep(1)
     player.stop()
